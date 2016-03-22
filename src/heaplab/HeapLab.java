bcc6ed00cbbce25f09bbad31e683ae21b7b95939
MistyMountainsCall <walkervanhouten00@gmail.com>
2016-03-22 14:52:35 -0700

    First revision. Need to change sort algorithm and add remove functions.

--- src/heaplab/HeapLab.java
+++ src/heaplab/HeapLab.java
@@ -10,7 +10,10 @@ public class HeapLab {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Heap h = new Heap();
+        int[] toAdd = {15, 5, 8, 4, 9, 22, 17, 6, 14};
+        h.add(toAdd);
+        h.print();
     }
     
 }

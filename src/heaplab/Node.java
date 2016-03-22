1f4830841cd823d6a0aa30658f2ea154e48e5d14
MistyMountainsCall <walkervanhouten00@gmail.com>
2016-03-22 14:54:45 -0700

    First revision. Need to change sort algorithm and add remove functions.

---
+++ src/heaplab/Node.java
@@ -0,0 +1,18 @@
+package heaplab;
+
+/**
+ *
+ * @author Walker VanHouten
+ */
+public class Node {
+    
+    private int data;
+    
+    Node(int data) {
+        this.data = data;
+    }
+    
+    int getData() {
+        return data;
+    }
+}

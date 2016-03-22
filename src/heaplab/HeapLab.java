669e8014ddc0568292a25d25a9309a3b6e235932
MistyMountainsCall <walkervanhouten00@gmail.com>
2016-03-22 12:16:32 -0700

    Maze solving algorithm initial commit.

---
+++ src/heaplab/HeapLab.java
@@ -0,0 +1,16 @@
+package heaplab;
+
+/**
+ *
+ * @author Walker VanHouten
+ */
+public class HeapLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
